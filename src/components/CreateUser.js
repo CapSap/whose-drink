154acc2877ab9a4d82bb3a19050eebe6bb7c3ffb
CapSap <cshotam@gmail.com>
2021-05-13 15:30:32 +1000

    intial and cleanup

---
+++ src/components/CreateUser.js
@@ -0,0 +1,19 @@
+function CreateUser() {
+  return(
+    <div>
+        <form>
+          <input 
+            type='text' 
+            placeholder='your name goes  here'
+            >
+          </input>
+          <button>
+            Submit
+          </button>
+      </form>
+    </div>
+  )
+
+}
+
+export default CreateUser
