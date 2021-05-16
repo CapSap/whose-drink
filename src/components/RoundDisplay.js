3537e477215a963462d82e1800f4713ea325eb5f
CapSap <cshotam@gmail.com>
2021-05-17 07:40:00 +1000

    added a space

--- src/components/RoundDisplay.js
+++ src/components/RoundDisplay.js
@@ -2,9 +2,8 @@ function RoundDisplay(props) {
   const roundArr = props.orderHistory.map((round, index) => (
     <div>
       <h3>Round {index + 1}:</h3>
-
-      <p>Buyer: {round.buyer}</p>
-      <p>Recievers: {round.receivers.map((receiver) => receiver)}</p>
+      <p>Buyer: {round.buyer ? round.buyer : "No-buyer"}</p>
+      <p>Recievers: {round.receivers.map((receiver) => " " + receiver )}</p>
     </div>
   ));
   return <div>{roundArr}</div>;
