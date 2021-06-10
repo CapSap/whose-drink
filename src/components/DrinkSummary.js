30b4ed3e06c6d23a5f923983b47554c1372545d8
CapSap <cshotam@gmail.com>
2021-06-10 21:20:13 +1000

    drinksum correctly cals

--- src/components/DrinkSummary.js
+++ src/components/DrinkSummary.js
@@ -1,8 +1,38 @@
 function DrinkSummary(props) {
-  const sum = props.orderHistory.map((x) => {
-    return x.receivers;
-  });
-  return <div>{sum}</div>;
+  let everyone = props.friendsArray.concat(props.userName);
+  let buyerSummary = [];
+
+  for (let i = 0; i < everyone.length; i++) {
+    let count = 0;
+
+    // for (let k = 0; k < props.orderHistory.length; k++) {
+    //   let { buyer, receivers } = props.orderHistory[k];
+    //   if (everyone[i] === buyer) {
+    //     for (let l = 0; l < receivers.length; l++) {
+    //       if (everyone[i] === receivers[l]) {
+    //         count++;
+    //       }
+    //     }
+    //   }
+    // }
+    // buyerSummary.push(
+    //   everyone[i] + " purchased " + count + " drink for themselves "
+    // );
+
+    props.orderHistory.forEach((x) => {
+      let { buyer, receivers } = x;
+      if (everyone[i] === buyer) {
+        for (let l = 0; l < receivers.length; l++) {
+          if (everyone[i] === receivers[l]) {
+            count++;
+          }
+        }
+        console.log(count);
+      }
+    });
+  }
+
+  return <div>{buyerSummary}</div>;
 }
 
 export default DrinkSummary;
