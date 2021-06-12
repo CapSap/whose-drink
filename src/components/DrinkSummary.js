c38b72e6c52b48c8c646459bc90d89eff6d7e3a3
DuskchatterX <68283828+DuskchatterX@users.noreply.github.com>
2021-06-12 22:45:56 +1000

    selfbuyer summary working!

--- src/components/DrinkSummary.js
+++ src/components/DrinkSummary.js
@@ -4,7 +4,8 @@ function DrinkSummary(props) {
   const [summaryState, setSummaryState] = useState([]);
 
   let everyone = props.friendsArray.concat(props.userName);
-  let buyerSummary = [];
+  let buyerSummary = {};
+  let render = [];
 
   for (let i = 0, n = everyone.length; i < n; i++) {
     let count = 0;
@@ -17,34 +18,21 @@ function DrinkSummary(props) {
             count++;
           }
         }
-        // console.log(
-        //   buyer + " purchase this many drinks for themselves:" + count
-        // );
-
-        // console.log(buyerSummary);
+        let obj = { [buyer]: count };
+        buyerSummary[buyer] = count;
       }
-
-      // issue im having is buyersum was getting a new obj pushed with every cycle of loop. i want to update value only, not add a new object
-      console.log(count);
-      let obj = { [buyer]: count };
-      console.log(obj);
-      console.log(buyerSummary);
     }
-
-    // buyerSummary.push(
-    //   everyone[i] + " purchased " + count + " drink for themselves "
-    // );
-    // if the setstate is in the above code block react doenst like it because it will trigger a re-redender loop
-    // maybe button on click get drink summary
-    // then on button click get drink summary again
-    // ie show no of rounds, who purchased drinks for who?
-
-    // or always show drink summary?
-    // how? this functional component gets props from app state. it also takes these props and make some stuff out of it. as app state gets updated it will be passed to thsi app
-    // so? on load / componentDidMount load up this comp with a variable that holds some info that we loop through state and pull out some data?
+  }
+  console.log(buyerSummary);
+  for (let key in buyerSummary) {
+    render.push(
+      <div>
+        {[key]} purchased this many drinks for themselves: {buyerSummary[key]}
+      </div>
+    );
   }
 
-  return <div>test</div>;
+  return <div>{render}</div>;
 }
 
 export default DrinkSummary;
