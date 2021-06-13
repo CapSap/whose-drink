b3619edc2162bcf7fe57106c68b9005c13f4da3b
CapSap <cshotam@gmail.com>
2021-06-13 12:44:28 +1000

    fix key error in roundDisplay

--- src/components/RoundDisplay.js
+++ src/components/RoundDisplay.js
@@ -1,13 +1,15 @@
 function RoundDisplay(props) {
   const roundArr = props.orderHistory.map((round, index) => {
     if (round.date === "") {
-      return <div>no recorded entries</div>;
+      return <div key="intial">no recorded entries</div>;
     } else {
       return (
-        <div>
-          <h3>Round {index + 1}:</h3>
-          <p>Buyer: {round.buyer ? round.buyer : "No-buyer"}</p>
-          <p>
+        <div key={round.date}>
+          <h3 key={round.date}>Round {index + 1}:</h3>
+          <p key={round.date}>
+            Buyer: {round.buyer ? round.buyer : "No-buyer"}
+          </p>
+          <p key={round.date}>
             Receiver(s): {round.receivers.map((receiver) => " " + receiver)}
           </p>
         </div>
