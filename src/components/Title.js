e1dd3cfb2937a681c6d6891cc91969b936994c19
DuskchatterX <68283828+DuskchatterX@users.noreply.github.com>
2021-06-20 22:54:04 +1000

    title css tweak

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,8 @@
 function Title() {
   return (
     <div className="title">
-      <h1>Drink recorder</h1>
-      <p className="title-text">Track drink purchases below</p>
-      <p className="title-text">
-        Start by entering your name and the names of your buddies
-      </p>
+      <h1>Whose round is it anyway?</h1>
+      <h3 className="title-text">Track drink purchases below</h3>
     </div>
   );
 }
